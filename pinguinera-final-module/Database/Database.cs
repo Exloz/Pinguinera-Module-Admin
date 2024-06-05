68f310fae01b58bb8fdedf7702771104f48557b2
Exloz <exloz@hotmail.com>
2024-06-05 12:18:05 -0500

    Create Database files config file and class

--- pinguinera-final-module/Database/Database.cs
+++ pinguinera-final-module/Database/Database.cs
@@ -1,5 +1,22 @@
-﻿namespace pinguinera_final_module.Database;
+﻿using Microsoft.EntityFrameworkCore;
+using pinguinera_final_module.Database.Interfaces;
 
-public class Database {
-    
+namespace pinguinera_final_module.Database;
+
+public class Database : DbContext, IDatabase{
+
+    public Database(DbContextOptions options) : base(options) {
+    }
+
+    public async Task<bool> SaveAsync() {
+        return await SaveChangesAsync() > 0;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        EntityConfiguration(modelBuilder);
+    }
+
+    private void EntityConfiguration(ModelBuilder modelBuilder) {
+    }
 }
