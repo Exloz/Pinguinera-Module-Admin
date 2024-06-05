63dd86e6490168b55b98ad014e86eef5554ca132
Exloz <exloz@hotmail.com>
2024-06-05 16:38:23 -0500

    Create user service interface and his implementation

---
+++ Pinguinera.Module.Admin/Services/UserService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pinguinera_final_module.Database.Interfaces;
+using pinguinera_final_module.Models.Persistence;
+using pinguinera_final_module.Models.Repositories;
+
+namespace pinguinera_final_module.Services;
+
+public class UserService : IUserService {
+
+    private readonly IDatabase _userRepository;
+
+    public UserService(IDatabase userRepository) {
+        _userRepository = userRepository;
+    }
+
+    public async Task<List<User>> GetAllUser() {
+        return await _userRepository.Users.ToListAsync();
+    }
+}
