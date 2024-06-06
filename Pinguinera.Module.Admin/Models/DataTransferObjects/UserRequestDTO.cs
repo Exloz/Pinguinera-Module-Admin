fc2f0c9f537f4bd0307e803bb5dcbc2ed9d15974
Exloz <exloz@hotmail.com>
2024-06-05 21:22:29 -0500

    Create Mapper and Request DTO

---
+++ Pinguinera.Module.Admin/Models/DataTransferObjects/UserRequestDTO.cs
@@ -0,0 +1,11 @@
+﻿using pinguinera_final_module.Shared.Enums;
+
+namespace pinguinera_final_module.Models.DataTransferObjects;
+
+public class UserRequestDTO {
+
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public RoleType Role { get; set; }
+}
