c300d47d585b0af0c79feaa944be59d205f011f3
Exloz <exloz@hotmail.com>
2024-06-06 15:31:34 -0500

    Add validator to user controller and DTOs

--- Pinguinera.Module.Admin/Models/DataTransferObjects/UserUpdateDTO.cs
+++ Pinguinera.Module.Admin/Models/DataTransferObjects/UserUpdateDTO.cs
@@ -1,4 +1,5 @@
-﻿using pinguinera_final_module.Shared.Enums;
+﻿using FluentValidation;
+using pinguinera_final_module.Shared.Enums;
 
 namespace pinguinera_final_module.Models.DataTransferObjects;
 
@@ -6,4 +7,12 @@ public class UserUpdateDTO {
     public string Username { get; set; }
     public string Email { get; set; }
     public RoleType Role { get; set; }
+}
+
+public class UserUpdateDTOValidator : AbstractValidator<UserUpdateDTO> {
+    public UserUpdateDTOValidator() {
+        RuleFor(x => x.Username).NotEmpty().Length(3, 50);
+        RuleFor(x => x.Email).NotEmpty().EmailAddress();
+        RuleFor(x => x.Role).NotEmpty();
+    }
 }
