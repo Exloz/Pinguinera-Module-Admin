c62ea915ac570ef34e6d49fd7ca65a20e60ab42b
Exloz <exloz@hotmail.com>
2024-06-05 21:27:17 -0500

    Create user response DTO for endpoint

--- Pinguinera.Module.Admin/Models/DataTransferObjects/UserResponseDTO.cs
+++ Pinguinera.Module.Admin/Models/DataTransferObjects/UserResponseDTO.cs
@@ -1,5 +1,11 @@
-﻿namespace pinguinera_final_module.Models.DataTransferObjects;
+﻿using pinguinera_final_module.Shared.Enums;
+
+namespace pinguinera_final_module.Models.DataTransferObjects;
 
 public class UserResponseDTO {
-    
+    public Guid UserId { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public RoleType Role { get; set; }
 }
