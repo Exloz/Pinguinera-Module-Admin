5671d3d0be119ae0f745b3595396d9b08e939e1d
Exloz <exloz@hotmail.com>
2024-06-07 09:37:44 -0500

    Database connection updated

--- Pinguinera.Module.Admin/Database/Database.cs
+++ Pinguinera.Module.Admin/Database/Database.cs
@@ -53,7 +53,7 @@ public partial class Database : DbContext, IDatabase
     
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseNpgsql("Host=viaduct.proxy.rlwy.net;Port=59980;Database=railway;Username=postgres;Password=yYcfUDUZCdlmsMgWhCbGgsmQDjuShBZO");
+        => optionsBuilder.UseNpgsql("Host=roundhouse.proxy.rlwy.net;Port=53251;Database=railway;Username=postgres;Password=BowtptroFIuqlYAHdzutFUmNtVLouAfh");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
